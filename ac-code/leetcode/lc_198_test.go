aaf34a4cd4c9791598c541b09c306b7639daa628
CBH <changbaihenx@163.com>
2021-07-28 23:37:14 +0800

    update(ac-code): lc_198

--- ac-code/leetcode/lc_198_test.go
+++ ac-code/leetcode/lc_198_test.go
@@ -1,9 +1,15 @@
 package leetcode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRob(t *testing.T) {
-	Rob([]int{1})
+	arr := []int{1, 2, 3, 1}
+	fmt.Printf("输入：%+v\n输出：%d\n", arr, Rob(arr))
+	arr = []int{2, 7, 9, 3, 1}
+	fmt.Printf("输入：%+v\n输出：%d\n", arr, Rob(arr))
 }
 
 func Rob(nums []int) int {
