2409229085407f37acf131896d98d57f9aea5477
CBH <changbaihenx@163.com>
2021-07-30 12:01:22 +0800

    update(ac-code): lc_198

--- ac-code/leetcode/lc_198_test.go
+++ ac-code/leetcode/lc_198_test.go
@@ -7,11 +7,34 @@ import (
 
 func TestRob(t *testing.T) {
 	arr := []int{1, 2, 3, 1}
-	fmt.Printf("输入：%+v\n输出：%d\n", arr, Rob(arr))
+	fmt.Printf("输入：%+v\n", arr)
+	fmt.Printf("输出：%+v\n", Rob(arr))
 	arr = []int{2, 7, 9, 3, 1}
-	fmt.Printf("输入：%+v\n输出：%d\n", arr, Rob(arr))
+	fmt.Printf("输入：%+v\n", arr)
+	fmt.Printf("输出：%+v\n", Rob(arr))
 }
 
 func Rob(nums []int) int {
-	return 1
+	length := len(nums)
+	switch length {
+	case 0:
+		return 0
+	case 1:
+		return nums[0]
+	}
+	max := 0
+	for i := 0; i < length; i++ {
+		temp := 0
+		for j := i + 2; j < length; j++ {
+			p := Rob(nums[j:])
+			if p > temp {
+				temp = p
+			}
+		}
+		temp += nums[i]
+		if temp > max {
+			max = temp
+		}
+	}
+	return max
 }
