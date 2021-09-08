3bcae9adb93660dee5e5bf56d0debae2104407ea
changbaihe <changbaihenx@163.com>
2021-09-08 18:42:05 +0800

    update(ac-code): 完善leetcode/jzoffer/jz13

--- ac-code/leetcode/jzoffer/jz13/main.go
+++ ac-code/leetcode/jzoffer/jz13/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 /*
 剑指 Offer 13：机器人的运动范围
@@ -15,9 +18,122 @@ func main() {
 }
 func fmtMovingCount(m, n, k int) {
 	fmt.Printf("输入：m = %d, n = %d, k = %d\n", m, n, k)
-	fmt.Printf("输出：%+v\n", MovingCount(m, n, k))
+	fmt.Printf("输出：%+v\n", MovingCount2(m, n, k))
 }
 
-func MovingCount(m int, n int, k int) int {
-	return 1
+// 搜索实现
+func MovingCount(m, n, k int) int {
+	// 初始化：矩阵标记
+	mark := make([][]bool, m)
+	for i := 0; i < m; i++ {
+		mark[i] = make([]bool, n)
+	}
+	// return MoveDFS(m, n, k, 0, 0, &mark)
+	return MoveBFS(m, n, k, &mark)
+}
+
+// 位数求和
+func sum(x int) int {
+	count := 0
+	for x != 0 {
+		count += x % 10
+		x /= 10
+	}
+	return count
+}
+
+// DFS深搜
+func MoveDFS(m, n, k, x, y int, ptr_mark *[][]bool) int {
+	// 当前坐标出界了，不能搜索遍历，返回0
+	if x >= m || y >= n || x < 0 || y < 0 {
+		return 0
+	}
+	// 当前坐标走了，不用重复搜索遍历了，返回0
+	if (*ptr_mark)[x][y] {
+		return 0
+	}
+	// 当前坐标不满足条件，不用搜索遍历，返回0
+	if sum(x)+sum(y) > k {
+		return 0
+	}
+
+	// 标记走过当前坐标
+	(*ptr_mark)[x][y] = true
+
+	// 当前左边可以进行上左下右的搜索遍历, 初始化count
+	count := 1
+	// 只需要向两个方向去搜索就行，，，
+	// count += MoveDFS(m, n, k, x-1, y, ptr_mark)	// 向上遍历
+	// count += MoveDFS(m, n, k, x, y-1, ptr_mark)	// 向左遍历
+	count += MoveDFS(m, n, k, x+1, y, ptr_mark) // 向下遍历
+	count += MoveDFS(m, n, k, x, y+1, ptr_mark) // 向右遍历
+	return count
+}
+
+// BFS广搜
+func MoveBFS(m, n, k int, ptr_mark *[][]bool) int {
+	// 定义坐标
+	type site struct {
+		x, y int
+	}
+	count := 0 // 初始化计数器
+	queue := list.New()
+	queue.PushBack(site{0, 0}) // 起始坐标入队
+	for queue.Len() != 0 {
+		e := queue.Front()
+		s := queue.Remove(e).(site)
+		x, y := s.x, s.y
+		// 当前坐标出界了，不能搜索遍历，返回0
+		if x >= m || y >= n || x < 0 || y < 0 {
+			continue
+		}
+		// 当前坐标走了，不用重复搜索遍历了，返回0
+		if (*ptr_mark)[x][y] {
+			continue
+		}
+		// 当前坐标不满足条件，不用搜索遍历，返回0
+		if sum(x)+sum(y) > k {
+			continue
+		}
+		queue.PushBack(site{x, y + 1}) // 向右搜索
+		queue.PushBack(site{x + 1, y}) // 向下搜索
+
+		(*ptr_mark)[x][y] = true // 标记当前坐标已走过
+		count++                  // 计数器累计当前坐标
+	}
+	return count
+}
+
+// 递推实现
+func MovingCount2(m, n, k int) int {
+	// 初始化：递推矩阵
+	visit := make([][]int, m)
+	for i := 0; i < m; i++ {
+		visit[i] = make([]int, n)
+	}
+	visit[0][0] = 1 // 起始坐标标记
+	count := 1
+	for x := 0; x < m; x++ {
+		for y := 0; y < n; y++ {
+			// 起始坐标， 跳过
+			if x == 0 && y == 0 {
+				continue
+			}
+			// 当前坐标不满足条件，跳过
+			if sum(x)+sum(y) > k {
+				continue
+			}
+
+			if x-1 >= 0 {
+				visit[x][y] |= visit[x-1][y]
+			}
+			if y-1 >= 0 {
+				visit[x][y] |= visit[x][y-1]
+			}
+			if visit[x][y] == 1 {
+				count++
+			}
+		}
+	}
+	return count
 }
